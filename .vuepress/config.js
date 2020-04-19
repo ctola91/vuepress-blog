1d776d76eb79e4cba822fad76c243b5e8dd4b6ac
christian <christian@localhost.localdomain>
2020-04-18 23:19:31 -0400

    Add routing of My Blog

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "My Blog",
+  description: "This is a test of my blog",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Contact", link: "/contact" },
+      { text: "About Me", link: "/About" },
+    ],
+  },
+};
